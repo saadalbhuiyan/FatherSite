67480c095282aa4392efb9a7a5be27e5c02e25a1
saad bhuiyan <96346026+saadalbhuiyan@users.noreply.github.com>
2025-08-01 13:21:48 +0600

    multple admin login problem solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
+// ✅ Import Quill CSS
+import 'react-quill/dist/quill.snow.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
